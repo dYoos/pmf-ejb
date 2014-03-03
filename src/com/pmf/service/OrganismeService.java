c57588d3cf3a7ec1cf48d29be0f61cbd3b47b441
Youssef <yoos37@gmail.com>
2014-03-03 17:08:37 +0100

    OrganismeService added

---
+++ src/com/pmf/service/OrganismeService.java
@@ -0,0 +1,20 @@
+package com.pmf.service;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+
+/**
+ * Session Bean implementation class OrganismeService
+ */
+@Stateless
+@LocalBean
+public class OrganismeService {
+
+    /**
+     * Default constructor. 
+     */
+    public OrganismeService() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
